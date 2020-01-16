adcaa4d8748aaaa52a54255cea20df649d920a99
simon <ghbnty0202@gmail.com>
2020-01-16 15:57:03 +0900

    movie_app_2020

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from "axios";
 import Movie from "./Movie";
 import "./App.css";
-// import PropTypes from "prop-types"
 
 class App extends React.Component{
   state = {
@@ -26,13 +25,13 @@ class App extends React.Component{
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class = "container">
+      <section className="container">
         {isLoading ? (
          <div class="loader">
-            <span class="loader_text">Loading...</span>
+            <span className="loader_text">Loading...</span>
          </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => (
               <Movie
                 key={movie.id}
@@ -41,6 +40,7 @@ class App extends React.Component{
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
